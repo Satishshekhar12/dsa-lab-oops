f70a6f7e684b171c34b7a32298d29bb0563e7952
Satishshekhar12 <140783817+Satishshekhar12@users.noreply.github.com>
2025-04-02 15:55:06 +0530

    Update avlerror.cpp

--- avlerror.cpp
+++ avlerror.cpp
@@ -10,6 +10,7 @@ Welcome to GDB Online.
 using namespace std;
 
 class Node {
+    public:
     int data ,height ;
     Node * left, *right ;
     
@@ -18,36 +19,9 @@ class Node {
         left = right = NULL;
         height =1;
     }
-}
+};
 
-Node * left_rotation(Node * root){
-    Node *child = root->root->right;
-    Node * child_left = child->left;
-    
-    child->left= root;
-    root->right = child_left;
-     //now height calculation first roots than child
-    root->height= 1+(get_height(root->left),get_height(root->right));
-    child->height= 1+(get_height(child->left),get_height(child->right));
-    
-    return child;
-}
-
-Node * right_rotation(Node *root){
-    Node * child = root->left ;
-    child_right = child->right;
-    
-    child->right =root;
-    root->left = child;
-    
-    //now height calculation first roots than child
-    root->height= 1+(get_height(root->left),get_height(root->right));
-    child->height= 1+(get_height(child->left),get_height(child->right));
-    
-    return child;
-}
-
-int get_height(TreeNode *root){
+int get_height(Node *root){
     if(root==NULL) return 0;
     return root->height;
 }
@@ -55,16 +29,44 @@ int get_height(TreeNode *root){
 int get_balance(Node *root){
     return get_height(root->left) - get_height(root->right);
 }
+Node * left_rotation(Node * root){
+    Node *child = root->right;
+    Node * child_left = child->left;
+    
+    child->left= root;
+    root->right = child_left;
+     //now height calculation first roots than child
+    root->height= 1+max(get_height(root->left),get_height(root->right));
+    child->height= 1+max(get_height(child->left),get_height(child->right));
+    
+    return child;
+}
+
+Node * right_rotation(Node *root){
+    Node * child = root->left ;
+    Node *child_right = child->right;
+    
+    child->right =root;
+    root->left = child;
+    
+    //now height calculation first roots than child
+    root->height= 1+max(get_height(root->left),get_height(root->right));
+    child->height= 1+max(get_height(child->left),get_height(child->right));
+    
+    return child;
+}
+
+
 Node * insert(Node *root ,int val){
     if(root==NULL) return new Node(val);
     
     if(val < root->data ){
         root =insert(root->left,val);
     }
-    else (val > root -> data) {
-        root =insert (root->right ,val)
+    else if(val > root -> data) {
+        root =insert (root->right ,val);
     }
-    else return root;
+    else { return root; }
     
     root->height= 1+max(get_height(root->left),get_height(root->right)) ;
     
@@ -93,21 +95,21 @@ Node * insert(Node *root ,int val){
     //LR
     else if( balance >1 && root->left->data < val){
         root->left = left_rotation(root->left);
-        right_rotation (root);
+        return right_rotation (root);
     }
     //RL
     else if(balance < -1 && root->right->data > val){
         root->right=right_rotation(root->right);
-        left_rotation(root);
+        return left_rotation(root);
     }
     //not required to balance
-    else {
+    
         return root;
-    }
+    
 }
 
 void inorder (Node *root){
-    if(root==NULL) return NULL;
+    if(root==NULL) return ;
     inorder(root->left);
     cout<<root->data<<", ";
     inorder(root->right);
